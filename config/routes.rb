fe33156d092711ad0463c9c3d8db00c180bce095
warichan <txaxmxmxm@gmail.com>
2016-06-01 18:04:50 +0900

    新規登録ページを作成

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   get 'login' => 'user_sessions#new', :as => :login
   post 'logout' => 'user_sessions#destroy', :as => :logout
+  get 'sign_up' => 'users#new', :as => :sign_up
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
